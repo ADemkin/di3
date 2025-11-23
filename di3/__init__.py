18a87385e33217f26dba06aaac60344ec2ed740e
demkin.anton <demkin.anton@rwb.ru>
2025-11-23 20:45:44 +0200

    Add __all__

--- di3/__init__.py
+++ di3/__init__.py
@@ -1,3 +1,9 @@
 from di3.errors import DependencyInjectionError as DependencyInjectionError
 from di3.provider import Params as Params
 from di3.provider import Provider as Provider
+
+__all__ = [
+    "DependencyInjectionError",
+    "Params",
+    "Provider",
+]
